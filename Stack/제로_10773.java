70a6329678211032f9b9c560ea92ac734433f640
khg <pkgprud0707@pukyong.ac.kr>
2024-02-10 23:47:10 +0900

    재확인

--- Stack/제로_10773.java
+++ Stack/제로_10773.java
@@ -7,7 +7,7 @@ public class 제로_10773 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int K = Integer.parseInt(br.readLine());
         Deque<Integer> stack = new ArrayDeque<>();
-        for(int i=0;i<K;i++){
+        for(int i = 0;i<K;i++){
             int temp = Integer.parseInt(br.readLine());
             if(temp == 0) stack.pop();
             else stack.push(temp);
