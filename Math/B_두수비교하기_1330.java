8e0e60297287f3ed712b2d78c21cddd5bfb625f4
khg <pkgprud0707@pukyong.ac.kr>
2024-03-03 17:40:52 +0900

    20240303

---
+++ Math/B_두수비교하기_1330.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_두수비교하기_1330 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        if(A<B) System.out.println("<");
+        else if(A>B) System.out.println(">");
+        else System.out.println("==");
+
+    }
+}
