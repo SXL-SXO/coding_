92e3aafbf357d0f44c3e63f167bee9646ec04593
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-04-14 11:04:26 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 17528 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int K, N;
+    static int len[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+    
+        st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        len = new int[K];
+
+        long max = 0, min = 0 ;
+        for(int i=0;i<K;i++){
+            len[i] = Integer.parseInt(br.readLine());
+            max += (long)len[i];
+        }
+        min = 1; max/=N;
+        while(min<=max){
+            long mid = (min+max)/2L;
+            int count = 0;
+            for(int i=0;i<K;i++){
+                count += len[i]/mid;
+            }
+            if(count<N) { 
+                max = mid-1; 
+            }
+            else { 
+                min = mid+1;
+            }
+        }
+        System.out.println(min-1);
+    }
+}
