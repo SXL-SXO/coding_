1fb359c9d3bf23a5706839f6d15255ad82ac3360
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-04-17 17:53:31 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for(int i=1;i<=N;i++){
+            int num = i;
+            while(true){
+                if(num%5!=0) break;
+                answer+=1;
+                num=num/5;
+            }
+        }
+        System.out.println(answer);
+    }
+}
