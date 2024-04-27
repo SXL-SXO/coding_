235183b06345836f66f490cc3993c178b2b44e07
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-04-27 21:39:05 +0900

    [Bronze V] Title: 콜센터, Time: 112 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.print(
+            "     /~\\\n" + //
+                                "    ( oo|\n" + //
+                                "    _\\=/_\n" + //
+                                "   /  _  \\\n" + //
+                                "  //|/.\\|\\\\\n" + //
+                                " ||  \\ /  ||\n" + //
+                                "============\n" + //
+                                "|          |\n" + //
+                                "|          |\n" + //
+                                "|          |"
+        );
+    }
+}
