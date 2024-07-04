ea2fa5945b19088a525c071a8cc7d7aaad24e3db
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-07-04 09:38:32 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 256 ms, Memory: 18388 KB -BaekjoonHub

--- Java/백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ Java/백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -2,61 +2,65 @@ import java.util.*;
 import java.io.*;
 public class Main
 {
-    static PriorityQueue<Node> queue = new PriorityQueue<>((o1,o2)->(o1.t-o2.t));
+    static Queue<Integer> queue = new ArrayDeque<>();
     static int N, K;
-    static boolean visit[];
+    static int before[];
+    static int count[];
     static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-		if(N>=K){
-		    sb.append(N-K).append("\n");
-		    for(int i=N;i>=K;i--){
-		        sb.append(i).append(" ");
-		    }
-		    System.out.print(sb);
-		    return;
-		}
-		
-		visit = new boolean[K*2+1];
-		
-		queue.offer(new Node(0, N, ""+N));
-		
-		Node temp, answer;
-		while(!queue.isEmpty()){
-		    temp = queue.poll();
-		    if(visit[temp.i]) continue;
-		    visit[temp.i] = true;
-		  //  System.out.print(temp.i+" "+temp.t+" / ");
-		    if(temp.i == K){
-		        sb.append(temp.t).append("\n").append(temp.ans);
-		        break;
-		    }
-		    if(temp.i!=0 && temp.i<K && !visit[temp.i*2]){
-                queue.offer(new Node(temp.t+1, temp.i*2, temp.ans+" "+(temp.i*2)));
-		    }
-            if(temp.i<K && !visit[temp.i+1]){
-                queue.offer(new Node(temp.t+1, temp.i+1, temp.ans+" "+(temp.i+1)));
+        if(N>=K){
+            sb.append(N-K).append("\n");
+            for(int i=N;i>=K;i--){
+                sb.append(i).append(" ");
             }
-            if(temp.i>0 && !visit[temp.i-1]){
-                queue.offer(new Node(temp.t+1, temp.i-1, temp.ans+" "+(temp.i-1)));
+            System.out.print(sb);
+            return;
+        }
+        
+        before = new int[K*2+1];
+        count = new int[K*2+1];
+        
+        queue.offer(N);
+        
+        int temp;
+        before[N] = -1;
+        count[N] = 1;
+        while(!queue.isEmpty()){
+            temp = queue.poll();
+
+            if(temp == K){
+                int b = temp;
+                for(int j=count[temp]; j>0; j--){
+                    sb.insert(0, " ").insert(0, b);
+                    b = before[b];
+                }
+                
+                sb.insert(0, "\n").insert(0, count[temp]-1);
+                break;
             }
-		}
-		
-		System.out.print(sb);
-	}
-	static class Node{
-	    int t, i;
-	    String ans;
-	    Node(int t, int i, String ans){
-	        this.t = t;
-	        this.i = i;
-	        this.ans = ans;
-	    }
-	    
-	}
+            if(temp!=0 && temp<K && count[temp*2]==0){
+                queue.offer(temp*2);
+                before[temp*2] = temp; 
+                count[temp*2] = count[temp]+1;
+            }
+            if(temp<K && count[temp+1]==0){
+                queue.offer(temp+1);
+                before[temp+1] = temp;
+                count[temp+1] = count[temp]+1;
+            }
+            if(temp>0 && count[temp-1]==0){
+                queue.offer(temp-1);
+                before[temp-1] = temp;
+                count[temp-1] = count[temp]+1;
+            }
+        }
+        
+        System.out.print(sb);
+    }
 }
