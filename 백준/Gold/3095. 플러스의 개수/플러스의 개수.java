a76b5336205b70b66362151eb5e7acdf33e76d46
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-07-08 22:16:13 +0900

    [Gold IV] Title: 플러스의 개수, Time: 364 ms, Memory: 40436 KB -BaekjoonHub

--- 백준/Gold/3095. 플러스의 개수/플러스의 개수.java
+++ 백준/Gold/3095. 플러스의 개수/플러스의 개수.java
@@ -17,43 +17,33 @@ class Main{
 
         for(int i=1;i<N-1;i++){
             for(int j=1;j<N-1;j++){
-                if(visit[i][j]) continue;
-                if(map[i][j]=='1' && map[i-1][j]=='1' && map[i][j-1]=='1' && map[i+1][j]=='1' && map[i][j+1]=='1' ) {
-                    if (map[i - 1][j - 1] == '1' || map[i - 1][j + 1] == '1' || map[i + 1][j - 1] == '1' || map[i + 1][j + 1] == '1')
-                        continue;
-                    answer++;
-                    check(i, j);
-                }
+                if(visit[i][j] || map[i][j]!='1' || map[i-1][j]!='1' || map[i][j-1]!='1' || map[i+1][j]!='1' || map[i][j+1]!='1' || map[i - 1][j - 1] == '1' || map[i - 1][j + 1] == '1' || map[i + 1][j - 1] == '1' || map[i + 1][j + 1] == '1')
+                    continue;
+                answer++;
+                check(i, j);            
             }
         }
 
         System.out.println(answer);
     }
     static void check(int i, int j){
-        int ny, nx, mul=2;
-        total : for(;;mul++){
-            if(i-mul<0 || j-mul<0 || i+mul>=N || j+mul>=N) break;
-            for(int a=i-mul;a<=i+mul;a++){
-
-                if(a==i) continue;
-//                System.out.print(a+" "+(j-mul)+" / ");
-//                System.out.println(a+" "+(j+mul)+" / ");
-                if(map[a][j-mul] != '0' || map[a][j+mul] != '0') break total;
-            }
-//            System.out.println("행가능");
-            for(int a=j-mul;a<j+mul;a++){
-                if(a==j) continue;
-                if(map[i-mul][a] != '0' || map[i+mul][a] != '0') break total;
-            }
-//            System.out.println("열가능");
-
-            answer++;
-        }
-        mul--;
-        for(int a=i-mul;a<=i+mul;a++){
-            for(int b=j-mul;b<=j+mul;b++){
+        for(int a=i-1;a<=i+1;a++){
+            for(int b=j-1;b<=j+1;b++){
                 visit[a][b] = true;
             }
+        }
+        total : for(int mul = 2;;mul++){
+            if(i-mul<0 || j-mul<0 || i+mul>=N || j+mul>=N) break;
+            for(int a=-mul;a<=mul;a++){
+                if(a!=0 && ((map[i+a][j-mul] != '0' || map[i+a][j+mul] != '0') || (map[i-mul][j+a] != '0' || map[i+mul][j+a] != '0'))) break total;
+            }
+            for(int a=-mul;a<=mul;a++){
+                visit[i+a][j-mul] = true;
+                visit[i+a][j+mul] = true;
+                visit[i-mul][j+a] = true;
+                visit[i+mul][j+a] = true;
+            }
+            answer++;
         }
     }
 }
