2f4d3df434a3f7fe136cb9db3e50b41740dd435b
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-07-10 11:11:29 +0900

    [Gold IV] Title: 부분합, Time: 196 ms, Memory: 22640 KB -BaekjoonHub

---
+++ Java/백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+    static int input[];
+    static int N, S, start=-1, end, total = 0, answer = 0;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		
+		input = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++){
+		    input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=0;i<N;i++){
+		    total += input[i];
+		    
+		    if(total>=S) {
+        	    start = 0;
+        	    end = i;
+        	    // 앞쪽 줄여서 사이즈 최소로 만들기
+        	    smaller();
+        	    // 슬라이딩 윈도우로 체크하기
+		        check();
+		        break;
+		    }
+		}
+		
+		if(start==-1) answer = 0;
+		else answer = end-start;
+// 		start+" "+end+" "+
+		System.out.print(answer);
+	}
+	static void smaller(){
+	    for(;start<end;){
+	        total-=input[start++];
+	        if(total<S) {
+	            total+=input[--start];
+	            break;
+	        }
+	    }
+	}
+	static void check(){
+        end++;
+	    for(;end<N;){
+	        total -= input[start++];
+	        total += input[end];
+	        
+	        if(total>S) smaller();
+	        
+	        end++;
+	    }
+	}
+}
