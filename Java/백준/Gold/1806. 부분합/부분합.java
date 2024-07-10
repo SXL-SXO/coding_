b3a3d03585d9349463d2a73f70e76cba181b10ae
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-07-10 14:42:28 +0900

    [Gold IV] Title: 부분합, Time: 188 ms, Memory: 22040 KB -BaekjoonHub

--- Java/백준/Gold/1806. 부분합/부분합.java
+++ Java/백준/Gold/1806. 부분합/부분합.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main
 {
     static int input[];
-    static int N, S, answer = 0, start=-1, end, total = 0;
+    static int N, S, answer, start=0, total = 0;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -12,47 +12,21 @@ public class Main
 		S = Integer.parseInt(st.nextToken());
 		
 		input = new int[N];
+		answer = N+1;
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<N;i++){
 		    input[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i=0;i<N;i++){
-		    total += input[i];
-		    
-		    if(total>=S) {
-        	    start = 0;
-        	    end = i;
-        	    // 앞쪽 줄여서 사이즈 최소로 만들기
-        	    if(total>S) smaller();
-        	    // 슬라이딩 윈도우로 체크하기
-		        check();
-		        break;
-		    }
-		}
-		
-		if(start!=-1) answer = end-start;
+            total += input[i];
+            if(total>=S){
+        		while(total >= S){
+        			total -= input[start++];
+        		}
+    		    answer = Math.min(answer, i + 2 - start);
+    		}
+        }
+
+        if(answer == N+1) answer = 0;
 		System.out.print(answer);
-	}
-	static void smaller(){
-	    for(;start<end;){
-	        total-=input[start++];
-	        if(total<S) {
-	            total+=input[--start];
-	            break;
-	        }
-	    }
-	}
-	static void check(){
-        end++;
-	    for(;end<N;){
-	        total -= input[start++];
-	        total += input[end];
-	        
-	        if(total>S) smaller();
-	        
-	        end++;
-	    }
-	}
+    }
 }
