7bfd9e5fbea87d2e1885e420ff6b4cd6d9fb37a3
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-08-25 00:43:37 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 64 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -8,20 +8,18 @@ public class Main
     static String alpha[] = {"dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z="};
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    
-	    input = br.readLine();
-	    
-	    int len = input.length();
-        input = input.replaceAll(alpha[0], "0");
-        answer += (len-input.length())/2;
-	    
-	    for(int i=1;i<8;i++){
-	        len = input.length();
-            input = input.replaceAll(alpha[i], "0");
-            answer += (len-input.length());
-	    }
-	    	
-    	input = input.replaceAll("0", "");
-    	System.out.println(answer+input.length());
+		
+		input = br.readLine();
+		
+		int len = input.length();
+		int cnt = 0;
+		for(int i = 0; i < 8; i++){
+		    input = input.replace(alpha[i], ".");
+		    
+		    answer += (len - input.length()) / (alpha[i].length()-1);
+		    len = input.length();
+		}
+		input = input.replace(".", "");
+		System.out.println(answer + input.length());
 	}
 }
