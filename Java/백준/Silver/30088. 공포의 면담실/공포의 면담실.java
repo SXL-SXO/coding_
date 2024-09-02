f64a477060fe2d028f2f307fe9de42fb16f6923b
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-09-02 14:27:49 +0900

    [Silver V] Title: 공포의 면담실, Time: 76 ms, Memory: 11912 KB, Score: 36 point -BaekjoonHub

---
+++ Java/백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+    static int N, M, answer = 0, now = 0;
+    static int time[];
+	public static void main(String[] args) throws Exception {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    
+	    N = Integer.parseInt(br.readLine());
+	    time = new int[N];
+	    
+	    for(int i=0;i<N;i++){
+	        st = new StringTokenizer(br.readLine());
+	        M = Integer.parseInt(st.nextToken());
+	        for(int j=0;j<M;j++){
+	            time[i] += Integer.parseInt(st.nextToken());
+	        }
+	    }
+	    
+	    Arrays.sort(time);
+	    
+	    for(int i=0;i<N;i++){
+	        now += time[i];
+	        answer += now;
+	    }
+	    
+		System.out.print(answer);
+	}
+}
