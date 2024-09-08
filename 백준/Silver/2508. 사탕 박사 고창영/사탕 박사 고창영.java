1230e2a551f5cc7d51327a027f37b2aca664f20b
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-09-08 22:11:10 +0900

    [Silver V] Title: 사탕 박사 고창영, Time: 144 ms, Memory: 18852 KB -BaekjoonHub

---
+++ 백준/Silver/2508. 사탕 박사 고창영/사탕 박사 고창영.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static char map[][];
+    static int T, R, C, answer = 0;
+    static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    T = Integer.parseInt(br.readLine());
+	    
+	    for(int t=0;t<T;t++){
+	        br.readLine();
+	        st = new StringTokenizer(br.readLine());
+	        answer = 0;
+	        R = Integer.parseInt(st.nextToken());
+	        C = Integer.parseInt(st.nextToken());
+	        
+	        map = new char[R][];
+	        for(int i = 0;i<R; i++){
+	            map[i] = br.readLine().toCharArray();
+	        }
+	        
+	        for(int i=0;i<R;i++){
+	            for(int j=0;j<C;j++){
+	                if(map[i][j]=='o'){
+	                    if(i>=1 && i<R-1 && map[i-1][j]=='v' && map[i+1][j]=='^') answer++;
+	                    else if(j>=1 && j<C-1 && map[i][j-1]=='>' && map[i][j+1]=='<') answer++;
+	                }
+	            }
+	        }
+	        sb.append(answer).append("\n");
+	    }
+	    System.out.print(sb);
+	}
+}
