7478ef6860fab17f94dcd0328960751fe7d52b93
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-09-16 00:08:44 +0900

    [Gold V] Title: 시간 관리하기, Time: 192 ms, Memory: 18660 KB -BaekjoonHub

--- Java/백준/Gold/6068. 시간 관리하기/시간 관리하기.java
+++ Java/백준/Gold/6068. 시간 관리하기/시간 관리하기.java
@@ -3,26 +3,25 @@ import java.util.*;
 public class Main
 {
     static int N, size, answer = 0;
-    static PriorityQueue<Node> input = new PriorityQueue<>((o1, o2)-> o2.end-o1.end);
+    static Node input[];
     static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception{
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-
+        input = new Node[N];
         
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
-            input.offer(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            input[i] = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
-        answer = input.peek().end;
+        Arrays.sort(input, (o1,o2)->o2.end-o1.end);
+        answer = input[0].end;
         
-        Node temp;
         for(int i=0;i<N;i++){
-            temp = input.poll();
-            answer = Math.min(temp.end, answer);
-            answer -= temp.time;
+            answer = Math.min(input[i].end, answer);
+            answer -= input[i].time;
         }
         answer = answer<0 ? -1 : answer;
         System.out.print(answer);
