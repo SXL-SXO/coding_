5df082e81f384b1f271f37eca205a46c994f354b
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-10-21 23:30:30 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 68 ms, Memory: 11816 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		BigInteger N = new BigInteger(st.nextToken());
+		BigInteger M = new BigInteger(st.nextToken());
+
+		System.out.print(N.divide(M)+"\n"+N.remainder(M));
+	}
+}
