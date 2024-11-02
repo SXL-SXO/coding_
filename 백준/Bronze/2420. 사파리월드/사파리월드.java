96e27b9f2ed12a62fcb0fbdd81cabea6b0901699
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-11-02 15:28:57 +0900

    [Bronze V] Title: 사파리월드, Time: 88 ms, Memory: 12796 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.println(Math.abs(Long.parseLong(sc.next())- Long.parseLong(sc.next())));
+	}
+}
