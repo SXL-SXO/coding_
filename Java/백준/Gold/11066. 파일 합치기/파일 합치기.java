eee660e8f65f813e8c686e9755826b424a7c02f1
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-11-14 20:23:57 +0900

    [Gold III] Title: 파일 합치기, Time: 568 ms, Memory: 32644 KB -BaekjoonHub

--- Java/백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ Java/백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main
 {
     static int T, N;
-    static int input[], sum[][], dp[][];
+    static int sum[][], dp[][];
     static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,7 +12,6 @@ public class Main
 		
 		for(int t=0;t<T;t++){
 		    N = Integer.parseInt(br.readLine());
-		    input = new int[N];
 		    
 		    //시작인덱스. 포함되는 개수
 		    dp = new int[N][N+1];
@@ -20,7 +19,7 @@ public class Main
 		    
 		    st = new StringTokenizer(br.readLine());
 		    for(int i=0;i<N;i++){
-		        sum[i][1] = input[i] = Integer.parseInt(st.nextToken());
+		        sum[i][1] = Integer.parseInt(st.nextToken());
 		    }
 		    
 		    //개수
