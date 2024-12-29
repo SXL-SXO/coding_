e955fa080a5e36e97e81893df430453a72a3df08
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2024-12-29 23:10:34 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 68 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int K, N;
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	    K = Integer.parseInt(br.readLine());
+	    
+		System.out.println("1\n0");
+	}
+}
