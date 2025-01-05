00e56674dbfc412ee32ba6b55eab05ae74b8e1a8
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-01-05 20:10:25 +0900

    [Gold III] Title: ABBC, Time: 176 ms, Memory: 26816 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.java
+++ 백준/Gold/25381. ABBC/ABBC.java
@@ -5,7 +5,6 @@ public class Main
 {
     static Queue<Integer> a = new ArrayDeque<>(); 
     static Queue<Integer> b = new ArrayDeque<>(); 
-    static PriorityQueue<Integer> b_ = new PriorityQueue<>((o1, o2)-> o2-o1);
     static Queue<Integer> c = new ArrayDeque<>(); 
     static int answer = 0;
 	public static void main(String[] args) throws Exception {
@@ -21,33 +20,27 @@ public class Main
 	    }
 	    
 	    
-	    int tmp1, tmp2, bigger = -1;
+	    int tmp1, tmp2;
+	    
+	    // a-b 짝지어 제거
+	    while(!b.isEmpty() && !a.isEmpty()){
+	        tmp1 = a.poll();
+	        tmp2 = b.peek();
+	        
+	        if(tmp1<tmp2) {
+	            answer++;
+	            b.poll();
+	        }
+	    }
 	    
 	    // b-c 짝지어 제거
-	    while(!b.isEmpty() && !c.isEmpty()){
+	    while(!c.isEmpty() && !b.isEmpty()){
 	        tmp1 = b.poll();
 	        tmp2 = c.peek();
 	        
-	        
 	        if(tmp1<tmp2) {
+	            answer++;
 	            c.poll();
-	            answer++;
-	        }else b_.offer(tmp1);
-	    }
-	    
-	    // b 재정렬, 제일 큰 녀석 제일 앞인지 체크하는 용임
-	    while(!b.isEmpty()){
-	        b_.offer(b.poll());
-	    }
-	    
-	    // a-b 짝지어 제거
-	    while(!a.isEmpty() && !b_.isEmpty()){
-	        tmp1 = a.poll();
-	        tmp2 = b_.peek();
-	        
-	        if(tmp1<tmp2) {
-	            b_.poll();
-	            answer++;
 	        }
 	    }
 		System.out.println(answer);
