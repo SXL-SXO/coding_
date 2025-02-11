994500c49c91aa569cbbd90d81d4bc352f29dadd
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-02-11 18:30:45 +0900

    [Gold II] Title: Rush & Slash, Time: 908 ms, Memory: 142076 KB -BaekjoonHub

--- 백준/Gold/30622. Rush ＆ Slash/Rush ＆ Slash.java
+++ 백준/Gold/30622. Rush ＆ Slash/Rush ＆ Slash.java
@@ -3,10 +3,6 @@ import java.io.*;
 
 public class Main
 {
-    // 잡초를 좌표상에 표시
-    // 연결되는 잡초들을 표시
-    // queue에 넣어서 제일 마지막에 나오는 녀석을 제외하고 answer+=거리*2
-    // 마지막은 거리
     static long answer = 0L;
     static int N, count = 0, min_y = 0, max_y = 0, min_x = 0, max_x = 0;
     static Map<Node, Integer> map = new HashMap<>();
@@ -33,7 +29,6 @@ public class Main
 		    max_x = Math.max(max_x, input[i].x);
 		}
 		
-		// 각 연결된 잡초 체크
 		dis = new int[N+1];
 		for(Node n : input){
 		    if(map.getOrDefault(n, 0) == N+1) link(n);
@@ -43,15 +38,9 @@ public class Main
 		for(int i=0;i<N;i++) answer += (long)dis[i]*2;
 		answer += (long)dis[N];
 		
-// 		for(int i=0;i<N;i++){
-// 		    System.out.print(map.get(input[i])+" ");
-// 		}
-// 		System.out.println(Arrays.toString(dis));
-		
 		System.out.println(answer);
 	}
 	static void link(Node n){
-	    // 연결된 잡초의 최단거리
 	    dis[++count] = Math.abs(n.y)+Math.abs(n.x);
 	    queue.offer(n);
 	    
@@ -59,7 +48,6 @@ public class Main
 	    int ny, nx;
 	    while(!queue.isEmpty()){
 	        temp = queue.poll();
-	       // System.out.println("queue : "+temp.y+" "+temp.x);
 	        for(int i=0; i<8; i++){
 	            ny = temp.y+dy[i];
 	            nx = temp.x+dx[i];
