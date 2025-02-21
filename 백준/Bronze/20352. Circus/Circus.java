0fe06e5b82179159b97407dfeeaa9fe48937ff1a
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-02-22 04:31:57 +0900

    [Bronze IV] Title: Circus, Time: 104 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Bronze/20352. Circus/Circus.java
@@ -0,0 +1,7 @@
+import java.util.Scanner; 
+public class Main {	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);		
+        System.out.println(2*Math.pow(sc.nextDouble()*Math.PI, 0.5));	
+    }
+}
