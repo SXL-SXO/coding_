53a36dbe3f7f90c4d02eba745270e15f7508ac0b
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-04-01 18:57:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.93 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,35 +1,34 @@
+// 06:48~
 import java.util.*;
 class Solution {
-    static int dy[] = {-1,0,1,0};
-    static int dx[] = {0,1,0,-1};
-    static boolean visit[][];
+    static int dy[] = {-1, 0, 1, 0};
+    static int dx[] = {0, 1, 0, -1};
     static Queue<Integer> queue = new ArrayDeque<>();
-    public int solution(int[][] maps) {
-        int N = maps.length, M = maps[0].length;
+    static boolean visit[][];
+    static int N, M;
+    public int solution(int[][] input) {
+        N = input.length;
+        M = input[0].length;
         visit = new boolean[N][M];
         
-        int answer = 0, ny, nx, size, temp;
-        
         queue.offer(0);
-        visit[0][0] = true;
-        
-        while(!queue.isEmpty()){
-            size = queue.size();
+        int answer = 0, temp, size, ny, nx;
+        total : while(!queue.isEmpty()){
             answer++;
+            size = queue.size();
             for(int i=0;i<size;i++){
                 temp = queue.poll();
-                if(temp == N*M-1) {
-                    return answer;
-                }
+                if(temp == N*M-1) return answer;
                 for(int j=0;j<4;j++){
-                    ny = dy[j]+temp/M;
-                    nx = dx[j]+temp%M;
-                    if(ny>=N||ny<0||nx>=M||nx<0||maps[ny][nx]==0||visit[ny][nx]) continue;
+                    ny = temp/M+dy[j];
+                    nx = temp%M+dx[j];
+                    if(ny<0||ny>=N||nx<0||nx>=M||input[ny][nx]==0||visit[ny][nx]) continue;
                     visit[ny][nx] = true;
                     queue.offer(ny*M+nx);
                 }
             }
         }
+        
         return -1;
     }
 }
