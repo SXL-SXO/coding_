c4746ce42e4bb272c05415c5299de8ad5865bbd5
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-04-06 23:43:13 +0900

    [Silver IV] Title: 가로수, Time: 180 ms, Memory: 21512 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] streetTree =new int[N];
+
+        for(int i = 0; i < N; i++){
+            streetTree[i]=Integer.parseInt(br.readLine());
+        }
+
+        br.close();
+
+        int gcd = 0; //가로수 간격의 최대 공약수를 저장하는 변수
+
+        for(int i = 0; i < N-1; i++){
+            int distance = streetTree[i+1] - streetTree[i];
+            gcd = findGCD(distance, gcd); //가로수 간격의 최대 공약수
+        }
+        bw.write((streetTree[N-1]-streetTree[0])/gcd+1-(streetTree.length) + "");
+        bw.flush();
+        bw.close();
+
+    }
+    static int findGCD(int A, int B){
+        while(B != 0){
+            int R = A%B;
+            A = B;
+            B = R;
+        }
+        return A;
+    }
+}
