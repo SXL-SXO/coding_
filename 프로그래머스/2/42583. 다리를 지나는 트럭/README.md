f63904601872ac00777def2525c0fe72735b43e5
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-04-09 01:12:52 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 13.90 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,6 +1,6 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 09일 01:11:57
+2025년 04월 09일 01:12:48
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,36 +1,8 @@
-// 12:50
-import java.util.*;
+// 01:43
+
 class Solution {
-    static int N, T, W;
-    static int input[];
-    static Queue<Node> queue = new ArrayDeque<>();
-    public int solution(int b, int weight, int[] truck_weights) {
-        input = truck_weights;
-        N = input.length; 
-        T = b; // 걸리는 시간?
-        W = weight; // 다리 위 총 합 무게 
-        
-        int total = 0, t = 0, idx = 0, size;
-        
-        for(;idx<N;t++){
-            size = queue.size();
-            if(size>0 && queue.peek().time<=t) {
-                total -= input[queue.poll().idx];
-                size--;
-            }
-            if(size<T && total+input[idx]<=W) {
-                total += input[idx];
-                queue.offer(new Node(idx++, t+T));
-            }
-        }
-        t+=T;
-        return t;
-    }
-    static class Node{
-        int idx, time;
-        Node(int idx, int time) {
-            this.idx=idx;
-            this.time=time;
-        }
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
     }
 }
