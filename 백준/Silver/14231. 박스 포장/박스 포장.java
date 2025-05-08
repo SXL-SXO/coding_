6ee7a390831e1bf306a272d84d00bdb371dffb5b
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-05-09 00:09:09 +0900

    [Silver II] Title: 박스 포장, Time: 116 ms, Memory: 12152 KB -BaekjoonHub

--- 백준/Silver/14231. 박스 포장/박스 포장.java
+++ 백준/Silver/14231. 박스 포장/박스 포장.java
@@ -2,44 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
+	static int N, answer = 0;
 	static int[] box, dp;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine()); // 박스의 개수
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		
-		// 박스의 크기를 저장할 배열
-		box = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
+		N = Integer.parseInt(br.readLine());
+		
+		box = new int[N + 1];
+		dp = new int[N + 1];
+		dp[1] = 1;
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
 			box[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		// 박스를 겹칠때 겹쳐진 수를 표기하는 배열
-		dp = new int[n + 1];
-		dp[1] = 1;
-		
-		// find() 호출해서 가장 많이 들어가는 박스의 수를 찾는다.
-		for (int i = 2; i <= n; i++) {
+		for (int i = 2; i <= N; i++) {
 			dp[i] = find(i) + 1;
 		}
 		
-		// 가장 많이 겹쳐진 박스의 값을 출력
-		int count = 0;
 		for (int m : dp) {
-			count = Math.max(count, m);
+			answer = Math.max(answer, m);
 		}
 		
-		bw.write(Integer.toString(count));
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.print(answer);
 	}
 	
-	// 현재 박스에 가장 많이 들어갈 수 있는 박스의 수를 찾아주는 함수.
-	private static int find(int num) {
+	static int find(int num) {
 		int max = 0;
 		for (int i = num - 1; i > 0; i--) {
 			if (box[i] < box[num]) {
