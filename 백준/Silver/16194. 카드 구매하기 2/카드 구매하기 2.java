de21d16ca3c23b0fa69ae1b0add08cc7c3c148ba
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-08-08 19:52:15 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 88 ms, Memory: 11904 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int N;
+    static int input[], dp[];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        input = new int[N+1];
+        dp = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 1; i <= N; i++) dp[i] = input[i]*i;
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= i; j++){
+                dp[i] = Math.min(dp[i], dp[i-j]+input[j]);
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
