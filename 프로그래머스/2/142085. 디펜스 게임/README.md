70fe65e61c59d0a3b82d7f6dbc1de1742784447d
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-08-13 06:38:47 +0900

    [level 2] Title: 디펜스 게임, Time: 93.75 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/README.md
+++ 프로그래머스/2/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085?language=java) 
 
 ### 성능 요약
 
-메모리: 150 MB, 시간: 113.11 ms
+메모리: 128 MB, 시간: 93.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 01일 16:48:31
+2025년 08월 13일 06:38:45
 
 ### 문제 설명
 
--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,26 +1,22 @@
-// 04:04
+// 06:21
 import java.util.*;
-
 class Solution {
-    static int life, N, K;
+    static int N, K, answer = 0;
     static PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2-o1);
     public int solution(int n, int k, int[] enemy) {
-        life = n;
-        N = enemy.length;
-        K = k;
+        answer = N = enemy.length;
         
-        if(N<=K) return N;
-        
-        int answer = N, count = 0;
-        for(int i=0;i<N;i++) {
+        int life = n, K = k;
+        for(int i=0;i<N;i++){
             life -= enemy[i];
             pq.offer(enemy[i]);
             
-            while(life<0 && !pq.isEmpty() && count<K){
+            while(!pq.isEmpty() && life<0 && K>0) {
                 life += pq.poll();
-                count++;
+                K--;
             }
-            // System.out.println(life+" "+i+" "+count);
+            
+            // System.out.println(life +" "+ K);
             if(life<0) {
                 answer = i;
                 break;
@@ -28,7 +24,7 @@ class Solution {
         }
         
         
+        
         return answer;
     }
-    //dp k*len = 500_000_000_000 시간초과
 }
