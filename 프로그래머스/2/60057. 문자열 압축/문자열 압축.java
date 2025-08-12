309c6958760e9b386bfd7379eb9bba7cce78c5ab
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-08-13 07:00:04 +0900

    [level 2] Title: 문자열 압축, Time: 2.17 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,33 +1,45 @@
-// 08:43
+// 06:39~
+import java.util.*;
 class Solution {
     static int N, answer;
+    static StringBuilder sb = new StringBuilder();
     public int solution(String s) {
-        N = answer = s.length();
+        answer = N = s.length();
         
-        int count;
         String before, now;
-        StringBuilder sb = new StringBuilder();
+        int count;
         for(int i=N/2;i>0;i--){
+            sb.setLength(0);
+            
             now = s.substring(0, i);
             count = 1;
-            for(int j=i*2;j<=N;j+=i){
+            for(int j=i*2;j<N;j+=i){
                 before = now;
                 now = s.substring(j-i, j);
-                // System.out.println(before+" "+now);
+                
                 if(before.equals(now)) count++;
-                else{
-                    if(count == 1) sb.append(before);
-                    else sb.append(count).append(before);
+                else {
+                    sb.append(before);
                     
+                    if(count>1) sb.append(count);
                     count = 1;
                 }
             }
-            if(count == 1) sb.append(now);
-            else sb.append(count).append(now);
             
-            answer = Math.min(answer, sb.length()+N%i);
-            // System.out.println(answer+" "+sb);
-            sb.setLength(0);
+            before = now;
+            if(N % i == 0) now = s.substring(N - i);
+            else now = s.substring(N - N%i);
+            // System.out.print(before+" "+now+" ");
+            
+            if(before.equals(now)) sb.append(before).append(count+1);
+            else {
+                sb.append(before);
+                if(count>1) sb.append(count);
+                sb.append(now);
+            }
+            
+            // System.out.println(sb);
+            answer = Math.min(answer, sb.length());
         }
         
         return answer;
