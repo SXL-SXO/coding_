8ad90821bcd75e9519e3704936368d686f4834ca
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-08-13 07:33:31 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.14 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,36 +1,46 @@
+// 07:19
+import java.util.*;
+
 class Solution {
-    static int N, t, os, oe;
+    static int len, p, os, oe;
     static StringBuilder sb = new StringBuilder();
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        
         String temp[];
         
-        temp = video_len.split(":");
-        N = Integer.parseInt(temp[0])*60 + Integer.parseInt(temp[1]);
+        temp = video_len.split(":");        
+        len = Integer.parseInt(temp[0])*60;
+        len += Integer.parseInt(temp[1]);
         
-        temp = pos.split(":");
-        t = Integer.parseInt(temp[0])*60 + Integer.parseInt(temp[1]);
+        temp = pos.split(":");        
+        p = Integer.parseInt(temp[0])*60;
+        p += Integer.parseInt(temp[1]);
         
-        temp = op_start.split(":");
-        os = Integer.parseInt(temp[0])*60 + Integer.parseInt(temp[1]);
+        temp = op_start.split(":");        
+        os = Integer.parseInt(temp[0])*60;
+        os += Integer.parseInt(temp[1]);
         
-        temp = op_end.split(":");
-        oe = Integer.parseInt(temp[0])*60 + Integer.parseInt(temp[1]);        
+        temp = op_end.split(":");        
+        oe = Integer.parseInt(temp[0])*60;
+        oe += Integer.parseInt(temp[1]);
         
-        if(os <= t && t< oe) t = oe;
-        for(String c:commands){
-            if(c.equals("prev")) t -= 10;
-            else t += 10;
+        int now = p;
+        for(String c: commands){
+            if(os<=now && now<oe) now = oe;
             
-            if(t<0) t = 0;
-            else if(t>N) t = N;
+            if(c.equals("next")) now += 10;
+            else now -= 10;
             
-            if(os <= t && t < oe) t = oe;
+            if(now<0) now = 0;
+            else if(now>len) now = len;
         }
+        if(os<=now && now<oe) now = oe;
         
-        if(t/60 < 10) sb.append(0);
-        sb.append(t/60).append(":");
-        if(t%60 < 10) sb.append(0);
-        sb.append(t%60);
+        if(now/60 < 10) sb.append("0");
+        sb.append(now/60).append(":");
+        if(now%60 < 10) sb.append("0");
+        sb.append(now%60);
+        
         return sb.toString();
     }
 }
