1fe9f9d714c3441f49f121a32f0bd68ce32bad30
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-09-05 20:44:59 +0900

    [level 4] Title: 도둑질, Time: 30.96 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -16,28 +16,11 @@ class Solution {
         dp[0][1] = input[1];
         dp[1][1] = input[0];
         
-        dp[0][2] = Math.max(input[1], input[2]);
-        dp[1][2] = input[0]+input[2];
-        
-        for(int i=3;i<N-1;i++){
-            dp[0][i] = Math.max(dp[0][i-3], dp[0][i-2])+input[i];
-            dp[0][i] = Math.max(dp[0][i], dp[0][i-1]);
-            dp[1][i] = Math.max(dp[1][i-3], dp[1][i-2])+input[i];
-            dp[1][i] = Math.max(dp[1][i], dp[1][i-1]);
+        for(int i=2;i<N;i++){
+            dp[0][i] = Math.max(dp[0][i-2]+input[i], dp[0][i-1]);
+            dp[1][i] = Math.max(dp[1][i-2]+input[i], dp[1][i-1]);
         }
-        
-        if(N%2 == 1) {
-            dp[0][N-1] = Math.max(dp[0][N-4], dp[0][N-3])+input[N-1];
-            dp[0][N-1] = Math.max(dp[0][N-1], dp[0][N-2]);
-            dp[1][N-1] = Math.max(dp[1][N-4], dp[1][N-3]);
-            dp[1][N-1] = Math.max(dp[1][N-1], dp[1][N-2]);
-        }else{
-            dp[0][N-1] = Math.max(dp[0][N-4], dp[0][N-3])+input[N-1];
-            dp[0][N-1] = Math.max(dp[0][N-1], dp[0][N-2]);
-            dp[1][N-1] = Math.max(dp[1][N-4], dp[1][N-3]);
-            dp[1][N-1] = Math.max(dp[1][N-1], dp[1][N-2]);
-        }
-        answer = Math.max(dp[0][N-1], dp[1][N-1]);
+        answer = Math.max(dp[0][N-1], dp[1][N-2]);
         
         return answer;
     }
