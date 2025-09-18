ad448f482ac3c93c18b936d7079156d68d82c700
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-09-19 02:58:07 +0900

    [Gold III] Title: 가희와 탑, Time: 148 ms, Memory: 17396 KB -BaekjoonHub

--- 백준/Gold/24337. 가희와 탑/가희와 탑.java
+++ 백준/Gold/24337. 가희와 탑/가희와 탑.java
@@ -12,19 +12,11 @@ public class Main
 	    A = Integer.parseInt(st.nextToken());
 	    B = Integer.parseInt(st.nextToken());
 	    
-	    if(A+B-1 == N) {
-	        for(int i=1;i<A;i++) sb.append(i).append(" ");
-	        sb.append(Math.max(A, B)).append(" ");
-            for(int i=B-1;i>0;i--) sb.append(i).append(" ");
-	    }else if(A+B-1 > N) sb.append(-1);
-	   // else if(A==1 && B==1) for(int i=1;i<=N;i++) sb.append(1).append(" ");
-	    else if(A==1){
+	    if(A+B-1 > N) sb.append(-1);
+        else if(A==1){
 	        sb.append(B).append(" ");
 	        for(int i=1;i<=N-B;i++) sb.append(1).append(" ");
 	        for(int i=B-1;i>0;i--) sb.append(i).append(" ");
-	    }else if(B==1){
-	        for(int i=1;i<=N-A;i++) sb.append(1).append(" ");
-	        for(int i=1;i<=A;i++) sb.append(i).append(" ");
 	    }else if(A<B){
 	        for(int i=0;i<=N-A-B;i++) sb.append(1).append(" ");
 	        for(int i=1;i<A;i++) sb.append(i).append(" ");
