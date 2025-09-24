7020451933917ec7a25f31a0e541818ff36dbe21
SXL-SXO <86880813+SXL-SXO@users.noreply.github.com>
2025-09-24 18:08:11 +0900

    [level 2] Title: 하노이의 탑, Time: 4.76 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,25 +1,33 @@
-// 02:37~
-import java.util.*;
+//05:16
 class Solution {
-    // 5개 사이즈 옮기기(1-3) -> 4개 옮기기(1-2) + 1->3 + 4개옮기기(2-3);
-    static List<int[]> list = new ArrayList<>();
-    static int N;
+    static int N, idx = 0;
+    static int answer[][];
     public int[][] solution(int n) {
-        move(n, 1, 3);
-        N = list.size();
-        int answer[][] = new int[N][2];
-        for(int i=0;i<N;i++) answer[i] = list.get(i);
+        N = n;
+        answer = new int[(int)Math.pow(2, N)-1][2];
+        
+        make(1, 3, N);
+        
         return answer;
     }
-    
-    static void move(int size, int start, int end){
-        if(size == 1){
-            list.add(new int[]{start, end});
-            return;
-        }
+    static void make(int s, int e, int m){ 
+        if(m==0) return;
         
-        move(size-1, start, 6-start-end);
-        list.add(new int[]{start, end});
-        move(size-1, 6-end-start, end);
+        if(s == 1 && e == 2) make(s, 3, m-1);
+        else if(s == 1 && e == 3) make(s, 2, m-1);
+        else if(s == 2 && e == 1) make(s, 3, m-1);
+        else if(s == 2 && e == 3) make(s, 1, m-1);
+        else if(s == 3 && e == 1) make(s, 2, m-1);
+        else if(s == 3 && e == 2) make(s, 1, m-1);
+        
+        answer[idx][0] = s;
+        answer[idx++][1] = e;
+        
+        if(s == 1 && e == 2) make(3, e, m-1);
+        else if(s == 1 && e == 3) make(2, e, m-1);
+        else if(s == 2 && e == 1) make(3, e, m-1);
+        else if(s == 2 && e == 3) make(1, e, m-1);
+        else if(s == 3 && e == 1) make(2, e, m-1);
+        else if(s == 3 && e == 2) make(1, e, m-1);
     }
 }
